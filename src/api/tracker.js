a7af7946515e88442797d6cd5f20fddae90410e4
Aaron Nguyen <aaronnguyen.vic@gmail.com>
2020-06-16 18:15:54 +1000

    Add some screenshots

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,6 +1,6 @@
 // import axios from 'axios';
 
-export default "https://36a5033b.ngrok.io";
+export default 'https://1780235d86d4.ngrok.io';
 
 // axios isn't working on ngrok server ???
 // export default axios.create({
